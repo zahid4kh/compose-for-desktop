2a8f813e5e688687a422a910b7852a6481179f07
zahid4kh <halilzahid@gmail.com>
2025-05-10 04:00:37 +0200

    build settings

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import java.util.UUID
+
+plugins {
+    kotlin("jvm") version "2.1.20"
+    id("org.jetbrains.compose")
+    id("org.jetbrains.kotlin.plugin.compose")
+    kotlin("plugin.serialization") version "2.1.20"
+}
+
+group = "desktop"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+    implementation(compose.materialIconsExtended)
+
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.10.2")
+    implementation("io.insert-koin:koin-core:4.0.3")
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
+            packageName = "desktop"
+            packageVersion = "1.0.0"
+
+            linux{
+                shortcut = true
+            }
+
+            windows{
+                shortcut = true
+                dirChooser = true
+                menu = true
+                upgradeUuid = "run the 'generateUpgradeUuid' task and paste the generated UUID here only once"
+            }
+
+            macOS{
+                dockName = "Compose for Desktop"
+            }
+        }
+    }
+}
+
+tasks.register("generateUpgradeUuid") {
+    group = "help"
+    description = "Generates a unique UUID to be used for the Windows MSI upgradeUuid."
+    doLast {
+        println("--------------------------------------------------")
+        println("Generated Upgrade UUID (must be pasted in the upgradeUuid for windows block only once so the MSI installer recognizes the update and does the uninstall/install):")
+        println(UUID.randomUUID().toString())
+        println("--------------------------------------------------")
+    }
+}
