9d14720893d547db7326070fc754ef3c3defab1a
zahid4kh <halilzahid@gmail.com>
2025-05-10 04:01:23 +0200

    Main launcher

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,33 @@
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import theme.AppTheme
+import java.awt.Dimension
+
+import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.getKoin
+
+fun main() = application {
+    startKoin {
+        modules(appModule)
+    }
+
+    val viewModel = getKoin().get<MainViewModel>()
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        state = rememberWindowState(size = DpSize(800.dp, 600.dp)),
+        alwaysOnTop = true,
+        title = "Desktop App - Made with Compose for Desktop"
+    ) {
+        window.minimumSize = Dimension(800, 600)
+
+        AppTheme{
+            App(
+                viewModel = viewModel
+            )
+        }
+    }
+}
