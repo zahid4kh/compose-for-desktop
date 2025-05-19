b1dc086d3a189207ef780dad0da1811db36f6bf0
zahid4kh <halilzahid@gmail.com>
2025-05-19 23:19:45 +0200

    wizard: project generator and helpers

---
+++ docs/lib/types.ts
@@ -0,0 +1,18 @@
+export interface ProjectOptions {
+  appName: string;
+  packageName: string;
+  projectVersion: string;
+  windowWidth: string;
+  windowHeight: string;
+  includeRetrofit: boolean;
+  includeDeskit: boolean;
+  includeSQLDelight: boolean;
+  includeKtor: boolean;
+  includeDecompose: boolean;
+  includeImageLoader: boolean;
+  includePrecompose: boolean;
+  includeSentry: boolean;
+  includeMarkdown: boolean;
+  includeHotReload: boolean;
+  includeKotlinxDatetime: boolean;
+}
