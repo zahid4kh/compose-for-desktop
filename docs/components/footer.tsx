022dd99149e334fda8a4d2530db4ebf6a7ba7abc
zahid4kh <halilzahid@gmail.com>
2025-06-30 20:06:32 +0200

    upd footer

--- docs/components/footer.tsx
+++ docs/components/footer.tsx
@@ -2,8 +2,19 @@ export default function Footer() {
   return (
     <footer className="py-10 px-4 text-center border-t border-border">
       <p>
-        <span className="font-bold text-primary text-lg tracking-tight">Compose for Desktop Wizard </span>
-        is an open-source project. View it on{" "}
+        <span className="font-bold text-primary text-lg tracking-tight">
+          Compose for Desktop Wizard{" "}
+        </span>
+        is an open-source project made by{" "}
+        <a
+          href="https://github.com/zahid4kh"
+          target="_blank"
+          className="text-primary font-semibold hover:underline hover:text-purple-700 transition-colors"
+          rel="noreferrer"
+        >
+          zahid4kh
+        </a>
+        . View it on{" "}
         <a
           href="https://github.com/zahid4kh/compose-for-desktop"
           target="_blank"
@@ -14,5 +25,5 @@ export default function Footer() {
         </a>
       </p>
     </footer>
-  )
+  );
 }
