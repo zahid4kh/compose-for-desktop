0a38861a1c342a6de8cab2a4dbb741b2164897c9
zahid4kh <halilzahid@gmail.com>
2025-07-01 10:18:01 +0200

    new branch for desktop client module

---
+++ desktopclient/build.gradle.kts
@@ -0,0 +1,240 @@
+
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.compose.compiler.gradle.ComposeFeatureFlag
+import java.util.*
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.jetbrains.compose)
+    alias(libs.plugins.kotlin.plugin.compose)
+    alias(libs.plugins.kotlin.plugin.serialization)
+    alias(libs.plugins.hotReload)
+}
+
+group = "composefordesktop"
+version = "1.0.0"
+
+repositories {
+    maven { url = uri("https://jitpack.io") }
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+    implementation(compose.material3)
+    implementation(compose.components.resources)
+    implementation(compose.materialIconsExtended)
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.swing)
+    implementation(libs.koin.core)
+    implementation(libs.bundles.slf4j)
+
+    implementation("com.github.zahid4kh:deskit:1.2.0")
+    implementation("org.apache.commons:commons-compress:1.27.1")
+}
+
+compose.desktop {
+    application {
+        /*
+        Must match the annotation in Main.kt
+        @file:JvmName("Compose for Desktop Wizard").
+        This also sets the app's dock name on Linux.
+         */
+        mainClass = "ComposeforDesktopWizard"
+
+        nativeDistributions {
+            jvmArgs += listOf("-Dfile.encoding=UTF-8")
+
+            buildTypes.release.proguard {
+                configurationFiles.from("proguard-rules.pro")
+                isEnabled = true
+                obfuscate = false
+                optimize = true
+            }
+
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Exe)
+            packageName = "composefordesktop"
+            packageVersion = "1.0.0"
+
+            linux{
+                shortcut = true
+                iconFile.set(project.file("src/main/composeResources/drawable/linuxos.png"))
+                description = "Desktop client for Compose for Desktop Wizard"
+            }
+
+            windows{
+                shortcut = true
+                dirChooser = true
+                menu = true
+                upgradeUuid = null
+                iconFile.set(project.file("src/main/composeResources/drawable/windowsos.ico"))
+            }
+
+            macOS{
+                dockName = "Compose for Desktop Wizard"
+                iconFile.set(project.file("src/main/composeResources/drawable/macos.icns"))
+            }
+        }
+    }
+}
+
+compose.resources{
+    publicResClass = false
+    packageOfResClass = "composefordesktop.resources"
+    generateResClass = auto
+}
+
+composeCompiler {
+    featureFlags.add(ComposeFeatureFlag.OptimizeNonSkippingGroups)
+}
+
+
+tasks.register("generateUpgradeUuid") {
+    group = "help"
+    description = "Generates a unique UUID to be used for the Windows MSI upgradeUuid."
+    doLast {
+        println("--------------------------------------------------")
+        println("Generated Upgrade UUID (must be pasted in the upgradeUuid for windows block only once so the MSI installer recognizes the update and does the uninstall/install):")
+        println(UUID.randomUUID().toString())
+        println("--------------------------------------------------")
+    }
+}
+
+
+
+// only for LinuxOS
+val workDir = file("deb-temp")
+val packageName = "${compose.desktop.application.nativeDistributions.packageName}"
+val desktopRelativePath = "opt/$packageName/lib/$packageName-$packageName.desktop"
+val appDisplayName = "Compose for Desktop Wizard - Desktop Client"
+val mainClass = "${compose.desktop.application.mainClass}"
+
+fun promptUserChoice(): String {
+    println(
+        """
+        🧩 Which packaging task do you want to run?
+        1 = packageDeb (default)
+        2 = packageReleaseDeb
+        """.trimIndent()
+    )
+    print("👉 Enter your choice [1/2]: ")
+
+    return Scanner(System.`in`).nextLine().trim().ifEmpty { "1" }
+}
+
+tasks.register("addStartupWMClassToDebDynamic") {
+    group = "release"
+    description = "Finds .deb file, modifies .desktop with Name and StartupWMClass, and rebuilds it"
+
+    doLast {
+        val debRoot = file("build/compose/binaries")
+        if (!debRoot.exists()) throw GradleException("❌ Folder not found: ${debRoot}")
+
+        val allDebs = debRoot.walkTopDown().filter { it.isFile && it.extension == "deb" }.toList()
+        if (allDebs.isEmpty()) throw GradleException("❌ No .deb files found under ${debRoot}")
+
+        // picking the latest .deb file
+        val originalDeb = allDebs.maxByOrNull { it.lastModified() }!!
+        println("📦 Found deb package: ${originalDeb.relativeTo(rootDir)}")
+
+        val modifiedDeb = File(originalDeb.parentFile, originalDeb.nameWithoutExtension + "-wm.deb")
+
+        // cleaning up "deb-temp" folder, if exists
+        if (workDir.exists()) workDir.deleteRecursively()
+        workDir.mkdirs()
+
+        // Step 1: Extracting generated debian package
+        exec {
+            commandLine("dpkg-deb", "-R", originalDeb.absolutePath, workDir.absolutePath)
+        }
+
+        // Step 2: Modifying the desktop entry file
+        val desktopFile = File(workDir, desktopRelativePath)
+        if (!desktopFile.exists()) throw GradleException("❌ .desktop file not found: ${desktopRelativePath}")
+
+        val lines = desktopFile.readLines().toMutableList()
+
+        // Modifying the Name field (app's display name on dock)
+        var nameModified = false
+        for (i in lines.indices) {
+            if (lines[i].trim().startsWith("Name=")) {
+                lines[i] = "Name=$appDisplayName"
+                nameModified = true
+                println("✅ Modified Name entry to: $appDisplayName")
+                break
+            }
+        }
+
+        // adding Name field if it doesn't exist
+        if (!nameModified) {
+            lines.add("Name=$appDisplayName")
+            println("✅ Added Name entry: $appDisplayName")
+        }
+
+        // Checking StartupWMClass field
+        var wmClassModified = false
+        for (i in lines.indices) {
+            if (lines[i].trim().startsWith("StartupWMClass=")) {
+                if (lines[i] != "StartupWMClass=$mainClass") {
+                    lines[i] = "StartupWMClass=$mainClass"
+                    wmClassModified = true
+                    println("✅ Updated StartupWMClass entry to: $mainClass")
+                } else {
+                    println("ℹ️ StartupWMClass already correctly set to: $mainClass")
+                }
+                break
+            }
+        }
+
+        // Adding StartupWMClass if it doesn't exist
+        if (!lines.any { it.trim().startsWith("StartupWMClass=") }) {
+            lines.add("StartupWMClass=$mainClass")
+            println("✅ Added StartupWMClass entry: $mainClass")
+        }
+
+        // Writing changes back to file
+        desktopFile.writeText(lines.joinToString("\n"))
+
+        println("\n📄 Final .desktop file content:")
+        println("--------------------------------")
+        desktopFile.readLines().forEach { println(it) }
+        println("--------------------------------\n")
+
+
+        // Step 3: Repackaging the debian package back
+        exec {
+            commandLine("dpkg-deb", "-b", workDir.absolutePath, modifiedDeb.absolutePath)
+        }
+
+        println("✅ Done: Rebuilt with Name=$appDisplayName and StartupWMClass=$mainClass -> ${modifiedDeb.name}")
+    }
+}
+
+
+tasks.register("packageDebWithWMClass") {
+    group = "release"
+    description = "Runs packaging task (packageDeb or packageReleaseDeb), then adds StartupWMClass"
+
+    doLast {
+        val choice = promptUserChoice()
+
+        val packagingTask = when (choice) {
+            "2" -> "packageReleaseDeb"
+            else -> "packageDeb"
+        }
+
+        println("▶️ Running: ${packagingTask}")
+        gradle.includedBuilds.forEach { it.task(":${packagingTask}") } // just in case of composite builds
+
+        exec {
+            commandLine("./gradlew clean")
+            commandLine("./gradlew", packagingTask)
+        }
+
+        tasks.named("addStartupWMClassToDebDynamic").get().actions.forEach { it.execute(this) }
+    }
+}
---
+++ desktopclient/src/main/kotlin/ProjectInformationSection.kt
@@ -0,0 +1,106 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import composefordesktop.resources.Inter_VariableFont
+import composefordesktop.resources.Res
+import org.jetbrains.compose.resources.Font
+
+@Composable
+fun ProjectInformationSection(
+    state: ViewState,
+    onIntent: (ViewIntent) -> Unit
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(10.dp),
+        modifier = Modifier.padding(20.dp)
+    ) {
+        Text(
+            text = "PROJECT INFORMATION",
+            fontFamily = FontFamily(Font(
+                Res.font.Inter_VariableFont,
+                weight = FontWeight.Bold,
+            )),
+            fontSize = 20.sp
+        )
+        HorizontalDivider()
+
+        ProjectInfoItem(
+            itemTitle = "Application Name*",
+            placeholderText = "My Compose App",
+            value = state.appName,
+            onValueChange = { onIntent(ViewIntent.UpdateAppName(it)) },
+            subtitle = "The name of your application. Used for window title and app name"
+        )
+
+        ProjectInfoItem(
+            itemTitle = "Package Name*",
+            placeholderText = "myapp",
+            value = state.packageName,
+            onValueChange = { onIntent(ViewIntent.UpdatePackageName(it)) },
+            subtitle = state.packageNameError.ifEmpty {
+                "The package name of your application. E.g. codeeditor"
+            },
+            isError = state.packageNameError.isNotEmpty()
+        )
+
+        ProjectInfoItem(
+            itemTitle = "Version",
+            placeholderText = "1.0.0",
+            value = state.projectVersion,
+            onValueChange = { onIntent(ViewIntent.UpdateVersion(it)) },
+            subtitle = "The version of your application. Used for versioning your app"
+        )
+    }
+}
+
+@Composable
+fun ProjectInfoItem(
+    itemTitle: String = "",
+    placeholderText: String = "",
+    subtitle: String? = null,
+    value: String = "",
+    onValueChange: (String) -> Unit = {},
+    isError: Boolean = false
+) {
+    Column {
+        Text(
+            text = itemTitle,
+            fontFamily = FontFamily(
+                Font(
+                    Res.font.Inter_VariableFont,
+                    weight = FontWeight.Bold
+                )
+            ),
+            fontSize = 16.sp
+        )
+
+        OutlinedTextField(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier.fillMaxWidth(),
+            placeholder = { Text(text = placeholderText) },
+            shape = MaterialTheme.shapes.medium,
+            supportingText = {
+                Text(
+                    text = subtitle ?: "",
+                    textAlign = TextAlign.Start,
+                    modifier = Modifier.fillMaxWidth(),
+                    color = if (isError) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            },
+            isError = isError
+        )
+    }
+}
