cb7abacb4038c5f26102f9722e69ecf1a6c9af5e
zahid4kh <halilzahid@gmail.com>
2025-07-02 10:08:35 +0200

    new ViewEffect state

--- src/main/kotlin/ViewEffect.kt
+++ src/main/kotlin/ViewEffect.kt
@@ -1,4 +1,5 @@
 sealed class ViewEffect {
     data class ShowError(val message: String) : ViewEffect()
+    data class ShowSuccess(val message: String) : ViewEffect()
     data class ProjectGenerated(val filePath: String) : ViewEffect()
 }
